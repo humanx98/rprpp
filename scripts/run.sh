4d3ef693e6dae38b037b2690f2b27eea9da06a43
Maksym Sditanov <msditanov@200volts.com>
2024-02-09 16:35:02 +0200

    Scripts: user full path
    Run commands from scripts with relative path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,9 @@
-./build/apps/glfwapp/Debug/glfwapp.exe
+#!/bin/bash
+
+set -e
+
+script_dir=$(dirname $0)
+root_dir=$(realpath "$script_dir/..")
+
+"$root_dir/build/apps/glfwapp/Debug/glfwapp.exe"
 # ./build/apps/consoleapp/Debug/consoleapp.exe
