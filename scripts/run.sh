d635f7b497666ac8027d0f4f6d14e8b1ab409e29
Bohdan Semenov <bsv3@dsone.3ds.com>
2024-02-11 17:25:53 +0200

    remove unuse folder and remove comments

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,4 @@ script_dir=$(dirname $0)
 root_dir=$(realpath "$script_dir/..")
 
 "$root_dir/build/apps/glfwapp/Debug/glfwapp.exe"
-# ./build/apps/consoleapp/Debug/consoleapp.exe
+# "$root_dir/build/apps/consoleapp/Debug/consoleapp.exe"
