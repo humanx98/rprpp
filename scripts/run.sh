a35a8f71e283a3a7214c74cca3e946749f9fd085
Bohdan Semenov <bodyasemzen@gmail.com>
2024-02-20 13:52:07 +0200

    add configuration of build type in bash scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+
+build_type=""
+if [ -z "$1" ] 
+then
+    build_type="Debug"
+else
+    build_type=${1}
+fi
+
 script_dir=$(dirname $0)
 root_dir=$(realpath "$script_dir/..")
 
-"$root_dir/build/apps/glfwapp/Debug/glfwapp.exe"
-# "$root_dir/build/apps/consoleapp/Debug/consoleapp.exe"
+"$root_dir/build/apps/glfwapp/$build_type/glfwapp.exe"
+# "$root_dir/build/apps/consoleapp/$build_type/consoleapp.exe"
